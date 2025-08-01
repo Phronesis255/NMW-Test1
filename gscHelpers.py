e48521c59b8d3c3ccb202b9427aa421c291fe8d9
Mohammad-Ali Rahebi <41546646+Phronesis255@users.noreply.github.com>
2025-08-01 19:54:29 +0330

    debug line

--- gscHelpers.py
+++ gscHelpers.py
@@ -31,15 +31,13 @@ def attempt_silent_auth(client_id, scopes):
 
 def connect_to_search_console(access_token, refresh_token, client_id, client_secret, scopes):
     try:
-        print("hey")
+        st.write("hey")
         creds = Credentials(token=access_token,
                             refresh_token=refresh_token,
                             token_uri='https://oauth2.googleapis.com/token',
                             client_id=client_id,
                             client_secret=client_secret,
-                            scopes=scopes,
-                            revoke_token_endpoint='https://oauth2.googleapis.com/revoke',
-                            revocation_endpoint_auth_method='client_secret_post')  # Add this line
+                            scopes=scopes)  # Add this line
 
         service = build('searchconsole', 'v1', credentials=creds)
         print("Successfully connected to Google Search Console API.")
